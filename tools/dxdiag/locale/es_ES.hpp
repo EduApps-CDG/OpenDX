b93ead744f164eccc369a063f9c916a366195805
rroblf01 <rroblf01@estudiantes.unileon.es>
2023-10-04 08:58:27 +0200

    Lint es_ES.hpp file

--- tools/dxdiag/locale/es_ES.hpp
+++ tools/dxdiag/locale/es_ES.hpp
@@ -42,46 +42,46 @@ Translation_t Translation_esES() {
 	r.system_info_opendxVersion = (char*) "Versión de OpenDX";
 
 	//Tab Display
-    r.display_device_label = (char*) "Etiqueta del dispositivo de visualización";
-    r.display_device_manufacturer = (char*) "Fabricante del dispositivo de visualización";
-    r.display_device_chipType = (char*) "Tipo de chip del dispositivo de visualización";
-    r.display_device_dacType = (char*) "Tipo de DAC del dispositivo de visualización";
-    r.display_device_type = (char*) "Tipo de dispositivo de visualización";
-    r.display_device_mem = (char*) "Memoria del dispositivo de visualización";
-    r.display_device_videoMem = (char*) "Memoria de vídeo del dispositivo de visualización";
-    r.display_device_sharedMem = (char*) "Memoria compartida del dispositivo de visualización";
-    r.display_device_currDisplayMode = (char*) "Modo de visualización actual del dispositivo de visualización";
-    r.display_device_monitor = (char*) "Monitor del dispositivo de visualización";
-    r.display_driver_label = (char*) "Etiqueta del controlador de visualización";
-    r.display_driver_mainDriver = (char*) "Controlador principal del controlador de visualización";
-    r.display_driver_version = (char*) "Versión del controlador de visualización";
-    r.display_driver_date = (char*) "Fecha del controlador de visualización";
-    r.display_driver_whqlLogo = (char*) "Logotipo WHQL del controlador de visualización";
-    r.display_driver_d3dDdi = (char*) "D3D DDI del controlador de visualización";
-    r.display_driver_featureLevels = (char*) "Niveles de características del controlador de visualización";
-    r.display_driver_model = (char*) "Modelo del controlador de visualización";
-    r.display_features_label = (char*) "Etiqueta de las características de visualización";
-    r.display_features_ddAccel = (char*) "Aceleración DD de las características de visualización";
-    r.display_features_d3dAccel = (char*) "Aceleración D3D de las características de visualización";
-    r.display_features_agpAccel = (char*) "Aceleración AGP de las características de visualización";
-    r.display_notes_label = (char*) "Etiqueta de las notas de visualización";
+	r.display_device_label = (char*) "Etiqueta del dispositivo de visualización";
+	r.display_device_manufacturer = (char*) "Fabricante del dispositivo de visualización";
+	r.display_device_chipType = (char*) "Tipo de chip del dispositivo de visualización";
+	r.display_device_dacType = (char*) "Tipo de DAC del dispositivo de visualización";
+	r.display_device_type = (char*) "Tipo de dispositivo de visualización";
+	r.display_device_mem = (char*) "Memoria del dispositivo de visualización";
+	r.display_device_videoMem = (char*) "Memoria de vídeo del dispositivo de visualización";
+	r.display_device_sharedMem = (char*) "Memoria compartida del dispositivo de visualización";
+	r.display_device_currDisplayMode = (char*) "Modo de visualización actual del dispositivo de visualización";
+	r.display_device_monitor = (char*) "Monitor del dispositivo de visualización";
+	r.display_driver_label = (char*) "Etiqueta del controlador de visualización";
+	r.display_driver_mainDriver = (char*) "Controlador principal del controlador de visualización";
+	r.display_driver_version = (char*) "Versión del controlador de visualización";
+	r.display_driver_date = (char*) "Fecha del controlador de visualización";
+	r.display_driver_whqlLogo = (char*) "Logotipo WHQL del controlador de visualización";
+	r.display_driver_d3dDdi = (char*) "D3D DDI del controlador de visualización";
+	r.display_driver_featureLevels = (char*) "Niveles de características del controlador de visualización";
+	r.display_driver_model = (char*) "Modelo del controlador de visualización";
+	r.display_features_label = (char*) "Etiqueta de las características de visualización";
+	r.display_features_ddAccel = (char*) "Aceleración DD de las características de visualización";
+	r.display_features_d3dAccel = (char*) "Aceleración D3D de las características de visualización";
+	r.display_features_agpAccel = (char*) "Aceleración AGP de las características de visualización";
+	r.display_notes_label = (char*) "Etiqueta de las notas de visualización";
 
-    //Tab Sound
-    r.sound_device_label = (char*) "Etiqueta del dispositivo de sonido";
-    r.sound_device_name = (char*) "Nombre del dispositivo de sonido";
-    r.sound_device_hardwareId = (char*) "ID de hardware del dispositivo de sonido";
-    r.sound_device_manufacturerId = (char*) "ID de fabricante del dispositivo de sonido";
-    r.sound_device_productId = (char*) "ID de producto del dispositivo de sonido";
-    r.sound_device_type = (char*) "Tipo de dispositivo de sonido";
-    r.sound_device_default = (char*) "Dispositivo de sonido predeterminado";
-    r.sound_driver_label = (char*) "Etiqueta del controlador de sonido";
-    r.sound_driver_name = (char*) "Nombre del controlador de sonido";
-    r.sound_driver_version = (char*) "Versión del controlador de sonido";
-    r.sound_driver_date = (char*) "Fecha del controlador de sonido";
-    r.sound_driver_whqlLogo = (char*) "Logotipo WHQL del controlador de sonido";
-    r.sound_driver_otherFiles = (char*) "Otros archivos del controlador de sonido";
-    r.sound_driver_provider = (char*) "Proveedor del controlador de sonido";
-    r.sound_notes_label = (char*) "Etiqueta de las notas de sonido";
+	//Tab Sound
+	r.sound_device_label = (char*) "Etiqueta del dispositivo de sonido";
+	r.sound_device_name = (char*) "Nombre del dispositivo de sonido";
+	r.sound_device_hardwareId = (char*) "ID de hardware del dispositivo de sonido";
+	r.sound_device_manufacturerId = (char*) "ID de fabricante del dispositivo de sonido";
+	r.sound_device_productId = (char*) "ID de producto del dispositivo de sonido";
+	r.sound_device_type = (char*) "Tipo de dispositivo de sonido";
+	r.sound_device_default = (char*) "Dispositivo de sonido predeterminado";
+	r.sound_driver_label = (char*) "Etiqueta del controlador de sonido";
+	r.sound_driver_name = (char*) "Nombre del controlador de sonido";
+	r.sound_driver_version = (char*) "Versión del controlador de sonido";
+	r.sound_driver_date = (char*) "Fecha del controlador de sonido";
+	r.sound_driver_whqlLogo = (char*) "Logotipo WHQL del controlador de sonido";
+	r.sound_driver_otherFiles = (char*) "Otros archivos del controlador de sonido";
+	r.sound_driver_provider = (char*) "Proveedor del controlador de sonido";
+	r.sound_notes_label = (char*) "Etiqueta de las notas de sonido";
 
 	return r;
 }
