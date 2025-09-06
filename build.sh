128d2c248f568d6183a218fec089b45752594545
Eduardo P. Gomez <eduardoprocopiogomez@gmail.com>
2025-09-06 17:20:03 -0300

    added arch packages for installation

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # This file will install the required packages and build the project
 # Feel free to contribute it to suit your distro.
 
+set -e
+
+INSTALL_CMD="sudo apt install -y --fix-missing"
+PACKAGES=();
 UBUNTU_PACKAGES=(
     gcc
     cmake
@@ -10,26 +14,60 @@ UBUNTU_PACKAGES=(
     libgtk-4-dev
     rpm # For cpack RPM generation ("rpm-build" in some distros)
 );
+ARCH_PACKAGES=(
+    base-devel
+    gdb
+    gcc
+    cmake
+    make
+    libdrm
+    gtk4
+    rpm-tools # For cpack RPM generation ("rpm-build" in some distros)
+);
+DISTRO="ubuntu" #default
 
 # Check if package is installed
 function check_packages {
     TO_INSTALL=""
-    for pkg in "$@"; do
-        if ! dpkg -s $pkg > /dev/null 2>&1; then
-            TO_INSTALL="$TO_INSTALL $pkg"
-        fi
-    done
+    if [ "$DISTRO" == "ubuntu" ]; then
+        for pkg in "$@"; do
+            if ! dpkg -s $pkg > /dev/null 2>&1; then
+                TO_INSTALL="$TO_INSTALL $pkg"
+            fi
+        done
+    elif [ "$DISTRO" == "arch" ]; then
+        for pkg in "$@"; do
+            if ! pacman -Qi $pkg > /dev/null 2>&1; then
+                TO_INSTALL="$TO_INSTALL $pkg"
+            fi
+        done
+    fi
 
     if [ -n "$TO_INSTALL" ]; then
         echo "The following packages are not installed: $TO_INSTALL"
         echo "Installing missing packages..."
-        sudo apt install $TO_INSTALL --fix-missing -y
+        $INSTALL_CMD $TO_INSTALL
     fi
 }
 
 # Check if package is installed (Ubuntu)
 echo "Checking required packages..."
-check_packages "${UBUNTU_PACKAGES[@]}"
+
+#check distro (ID)
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO=$ID
+    #Arch based distros
+    if [[ "$ID" == "arch" || "$ID_LIKE" == *"arch"* ]]; then
+        INSTALL_CMD="sudo pacman -Syu --needed --noconfirm"
+        PACKAGES=("${ARCH_PACKAGES[@]}")
+    fi
+else
+    echo "Cannot determine the operating system. Exiting."
+    exit 1
+fi
+
+check_packages "${PACKAGES[@]}"
 
 # Build the project
 echo "Building the project..."
