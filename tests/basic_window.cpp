2dbb7f257a9ef4b50a3d2d62e5dc135c6f98439d
Eduardo P. Gomez <eduardoprocopiogomez@gmail.com>
2025-09-07 01:53:57 -0300

    reshaped sample to better fit in both oses

--- tests/basic_window.cpp
+++ tests/basic_window.cpp
@@ -7,6 +7,9 @@
  */
 
  //default values
+#include "gio/gio.h"
+#include "glib-object.h"
+#include "gtk/gtk.h"
 #define ODX_APP_NAME "DX9 Sample" //default app name
 #define ODX_APP_CLASS "OpenDX.DX9Sample" //app identifier
 #define ODX_ICON "/usr/share/icons/opendx.png" //relative, absolute or system path
@@ -20,25 +23,8 @@
 #include <stdio.h>
 #include <string.h>
 
-/**
- * OpenDX can convert MSVC's WinMain, but
- * it's not recommended as every other platform
- * uses main() and it's not portable.
- */
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-    // Create the Direct3D device
-    
-    // Create the window
-    HWND hWnd = CreateWindow(
-        "DX9 Window", "DX9 Window",
-        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        0, 0, 640, 480,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
-    );
-    
+//GtkWidget* == HWND
+static LPDIRECT3DDEVICE9 compatCode(HWND hWnd) {
     LPDIRECT3DDEVICE9 pDevice = NULL;
     D3DPRESENT_PARAMETERS pp;
     
@@ -60,39 +46,100 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     MSG msg;
     ZeroMemory(&msg, sizeof(msg));
 
+    // Clean up
+    //pDevice->Release(); //causes a segfault
+    //pD3D->Release();
+    //return msg.wParam;
+    return pDevice;
+}
+
+// Render the scene
+static void drawCall(LPDIRECT3DDEVICE9 pDevice) {
+    /*pDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
+    pDevice->BeginScene();
+    pDevice->EndScene();*/
+    pDevice->Present(NULL, NULL, NULL, NULL);
+}
+
+//is windows?
+#ifdef _WIN32
+/**
+ * OpenDX can convert MSVC's WinMain, but
+ * it's not recommended as it could cause every kind of issue.
+ * You can still use it for testing purposes.
+ */
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    // Create the Direct3D device
+    
+    // Create the window
+    HWND hWnd = CreateWindow(
+        "DX9 Window", "DX9 Window",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        0, 0, 640, 480,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    compatCode(hWnd);
+
+
     while (msg.message != WM_QUIT) {
         if (PeekMessage(&msg, hWnd, 0, 0, PM_REMOVE)) {
             //TranslateMessage(&msg);
             //DispatchMessage(&msg);
         } else {
-            // Render the scene
-            /*pDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
-            pDevice->BeginScene();
-            pDevice->EndScene();*/
-            pDevice->Present(NULL, NULL, NULL, NULL);
+            drawCall(pDevice);
         }
     }
 
-    // Clean up
-    //pDevice->Release(); //causes a segfault
-    //pD3D->Release();
     DestroyWindow(hWnd);
-    //return msg.wParam;
 
     return 0;
 }
+#endif // _WIN32
 
+
+#ifdef __linux__
+static void activate(GtkApplication* app, gpointer user_data);
 //Linux's main or MinGW's main:
 int main(int argc, char* argv[]) {
     //If you want to use WinMain (please dont)
     //you can pass it as a parameter:
-    OpenDX odx(
-        argc, argv, WinMain,
-        //Needed if you plan to use CreateWindow
-        new OpenDX_ApplicationConfig {
-            .ApplicationId = ODX_APP_CLASS
-        }
-    ); //or OpenDX odx(); if you want to use main only.
+    OpenDX odx = OpenDX(); //pass WinMain and appConfig if you would like to experimentally run a WinMain app
 
-    return odx.getReturnCode(); // 0 if WinMain is not passed.
+    GtkApplication* app = gtk_application_new(ODX_APP_CLASS, G_APPLICATION_DEFAULT_FLAGS);
+    g_application_register(G_APPLICATION(app), NULL, NULL);
+    GMainContext *context = g_main_context_default();
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+    int r = g_application_run(G_APPLICATION(app), argc, argv);
+
+    g_object_unref(app);
+
+
+    return r; //odx.getReturnCode(); // 0 if WinMain is not passed.
 }
+
+static void activate(GtkApplication* app, gpointer user_data) {
+    GtkWidget *window;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), ODX_APP_NAME);
+    gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
+    gtk_window_set_default_icon_name("opendx");
+
+    gtk_window_present(GTK_WINDOW(window));
+
+    LPDIRECT3DDEVICE9 pDevice = compatCode(window);
+
+    while (g_list_model_get_n_items (gtk_window_get_toplevels ()) > 0) {
+        GMainContext *context = g_main_context_default();
+        if (g_main_context_pending(context)) {
+            g_main_context_iteration(context, true);
+        }
+        drawCall(pDevice);
+    }
+}
+#endif //__linux__
